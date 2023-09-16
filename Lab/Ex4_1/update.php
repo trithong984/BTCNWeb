d9b8b0f4336bd927401570376bb015d694856aa0
trithong984 <trithong2004@example.com>
2023-09-16 23:59:25 +0700

    commit

---
+++ Lab/Ex4_1/update.php
@@ -0,0 +1,19 @@
+<?php
+require_once('connect_db.php');
+
+// Get IDs
+$product_id = filter_input(INPUT_POST, 'product_id', FILTER_VALIDATE_INT);
+$category_id = filter_input(INPUT_POST, 'category_id', FILTER_VALIDATE_INT);
+
+// Delete the product from the database
+if ($product_id != false && $category_id != false) {
+    $query = 'UPDATE products
+              SET productID = :product_id WHERE product_id = 2 ';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':product_id', $product_id);
+    $success = $statement->execute();
+    $statement->closeCursor();    
+}
+
+// Display the Product List page
+include('index.php');
