2b6d0740b0622c4e672daa6d8557cbf2905bbce4
trithong984 <trithong2004@example.com>
2023-09-17 00:04:48 +0700

    1

--- Lab/Ex4_1/update.php
+++ Lab/Ex4_1/update.php
@@ -7,10 +7,9 @@ $category_id = filter_input(INPUT_POST, 'category_id', FILTER_VALIDATE_INT);
 
 // Delete the product from the database
 if ($product_id != false && $category_id != false) {
-    $query = 'UPDATE products
-              SET productID = :product_id WHERE product_id = 2 ';
+    $query = "UPDATE products
+              SET code = '5789' WHERE product_id = 2 ";
     $statement = $db->prepare($query);
-    $statement->bindValue(':product_id', $product_id);
     $success = $statement->execute();
     $statement->closeCursor();    
 }
