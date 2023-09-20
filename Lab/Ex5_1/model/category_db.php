104ae3c2028b3fa96846fae6352be5ad884adba2
trithong984 <trithong2004@example.com>
2023-09-20 15:24:08 +0700

    1

--- Lab/Ex5_1/model/category_db.php
+++ Lab/Ex5_1/model/category_db.php
@@ -23,18 +23,6 @@ function get_category_name($category_id) {
     return $category_name;
 }
 
-function add_category($category_id) {
-    global $db;
-    $query = 'SELECT * FROM categories
-              WHERE categoryID = :category_id';    
-    $statement = $db->prepare($query);
-    $statement->bindValue(':category_id', $category_id);
-    $statement->execute();    
-    $category = $statement->fetch();
-    $statement->closeCursor();    
-    $category_name = $category['categoryName'];
-    return $category_name;
-}
 
 function add_category($category_id, $category_name) {
     global $db;
